e67bcb4e2799e292d04aa057024e943f0814c094
Erin Mounts <erinys62@gmail.com>
2014-09-08 10:57:39 -0700

    Initial Commit

---
+++ BridgeSDK/BridgeSDK.h
@@ -0,0 +1,19 @@
+//
+//  BridgeSDK.h
+//  BridgeSDK
+//
+//  Created by Erin Mounts on 9/8/14.
+//  Copyright (c) 2014 Sage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BridgeSDK.
+FOUNDATION_EXPORT double BridgeSDKVersionNumber;
+
+//! Project version string for BridgeSDK.
+FOUNDATION_EXPORT const unsigned char BridgeSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BridgeSDK/PublicHeader.h>
+
+
---
+++ BridgeSDKTests/BridgeSDKTests.m
@@ -0,0 +1,40 @@
+//
+//  BridgeSDKTests.m
+//  BridgeSDKTests
+//
+//  Created by Erin Mounts on 9/8/14.
+//  Copyright (c) 2014 Sage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface BridgeSDKTests : XCTestCase
+
+@end
+
+@implementation BridgeSDKTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
