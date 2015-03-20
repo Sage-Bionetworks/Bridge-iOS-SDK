1735853ee9333af5cccb33edb2dc90560c53374c
Erin Mounts <erinys62@gmail.com>
2015-03-20 10:39:51 -0700

    Update unit tests

--- BridgeSDKTests/SBBAuthManagerTests.m
+++ BridgeSDKTests/SBBAuthManagerTests.m
@@ -19,11 +19,13 @@
 - (void)setUp {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
+    gSBBAppStudy = @"test";
 }
 
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
+    gSBBAppStudy = nil;
 }
 
 - (void)testSignIn {
