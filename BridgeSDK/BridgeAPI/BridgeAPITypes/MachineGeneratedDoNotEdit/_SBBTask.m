0695edb143dccf4ce9e289bed4bb28f79f550291
Erin Mounts <erinys62@gmail.com>
2015-05-05 17:50:18 -0700

    In progress
    code-complete, utterly untested, no unit/integration tests yet.

---
+++ BridgeSDK/BridgeAPI/BridgeAPITypes/MachineGeneratedDoNotEdit/_SBBTask.m
@@ -0,0 +1,111 @@
+//
+//  SBBTask.m
+//
+//	Copyright (c) 2014, 2015 Sage Bionetworks
+//	All rights reserved.
+//
+//	Redistribution and use in source and binary forms, with or without
+//	modification, are permitted provided that the following conditions are met:
+//	    * Redistributions of source code must retain the above copyright
+//	      notice, this list of conditions and the following disclaimer.
+//	    * Redistributions in binary form must reproduce the above copyright
+//	      notice, this list of conditions and the following disclaimer in the
+//	      documentation and/or other materials provided with the distribution.
+//	    * Neither the name of Sage Bionetworks nor the names of BridgeSDk's
+//		  contributors may be used to endorse or promote products derived from
+//		  this software without specific prior written permission.
+//
+//	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+//	ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//	DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+//	DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//	(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+//	ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// DO NOT EDIT. This file is machine-generated and constantly overwritten.
+// Make changes to SBBTask.h instead.
+//
+
+#import "_SBBTask.h"
+#import "NSDate+SBBAdditions.h"
+
+@interface _SBBTask()
+
+@end
+
+@implementation _SBBTask
+
+- (id)init
+{
+	if((self = [super init]))
+	{
+
+	}
+
+	return self;
+}
+
+#pragma mark Scalar values
+
+#pragma mark Dictionary representation
+
+- (id)initWithDictionaryRepresentation:(NSDictionary *)dictionary
+{
+	if((self = [super initWithDictionaryRepresentation:dictionary]))
+	{
+
+        self.activity = [dictionary objectForKey:@"activity"];
+
+        self.expiresOn = [NSDate dateWithISO8601String:[dictionary objectForKey:@"expiresOn"]];
+
+        self.finishedOn = [NSDate dateWithISO8601String:[dictionary objectForKey:@"finishedOn"]];
+
+        self.guid = [dictionary objectForKey:@"guid"];
+
+        self.scheduledOn = [NSDate dateWithISO8601String:[dictionary objectForKey:@"scheduledOn"]];
+
+        self.startedOn = [NSDate dateWithISO8601String:[dictionary objectForKey:@"startedOn"]];
+
+        self.status = [dictionary objectForKey:@"status"];
+
+	}
+
+	return self;
+}
+
+- (NSDictionary *)dictionaryRepresentation
+{
+	NSMutableDictionary *dict = [NSMutableDictionary dictionaryWithDictionary:[super dictionaryRepresentation]];
+
+    [dict setObjectIfNotNil:self.activity forKey:@"activity"];
+
+    [dict setObjectIfNotNil:[self.expiresOn ISO8601String] forKey:@"expiresOn"];
+
+    [dict setObjectIfNotNil:[self.finishedOn ISO8601String] forKey:@"finishedOn"];
+
+    [dict setObjectIfNotNil:self.guid forKey:@"guid"];
+
+    [dict setObjectIfNotNil:[self.scheduledOn ISO8601String] forKey:@"scheduledOn"];
+
+    [dict setObjectIfNotNil:[self.startedOn ISO8601String] forKey:@"startedOn"];
+
+    [dict setObjectIfNotNil:self.status forKey:@"status"];
+
+	return dict;
+}
+
+- (void)awakeFromDictionaryRepresentationInit
+{
+	if(self.sourceDictionaryRepresentation == nil)
+		return; // awakeFromDictionaryRepresentationInit has been already executed on this object.
+
+	[super awakeFromDictionaryRepresentationInit];
+}
+
+#pragma mark Direct access
+
+@end
---
+++ BridgeSDK/BridgeAPI/SBBTaskManager.m
@@ -0,0 +1,111 @@
+//
+//  SBBTaskManager.m
+//  BridgeSDK
+//
+//  Created by Erin Mounts on 5/5/15.
+//
+//	Copyright (c) 2015, Sage Bionetworks
+//	All rights reserved.
+//
+//	Redistribution and use in source and binary forms, with or without
+//	modification, are permitted provided that the following conditions are met:
+//	    * Redistributions of source code must retain the above copyright
+//	      notice, this list of conditions and the following disclaimer.
+//	    * Redistributions in binary form must reproduce the above copyright
+//	      notice, this list of conditions and the following disclaimer in the
+//	      documentation and/or other materials provided with the distribution.
+//	    * Neither the name of Sage Bionetworks nor the names of BridgeSDk's
+//		  contributors may be used to endorse or promote products derived from
+//		  this software without specific prior written permission.
+//
+//	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+//	ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//	DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+//	DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//	(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+//	ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+
+#import "SBBTaskManager.h"
+#import "SBBComponentManager.h"
+#import "SBBAuthManager.h"
+#import "SBBObjectManager.h"
+#import "SBBBridgeObjects.h"
+#import "NSDate+SBBAdditions.h"
+
+@implementation SBBTaskManager
+
++ (instancetype)defaultComponent
+{
+    static SBBTaskManager *shared;
+    
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        shared = [self instanceWithRegisteredDependencies];
+    });
+    
+    return shared;
+}
+
+- (NSURLSessionDataTask *)getTasksUntil:(NSDate *)until withCompletion:(SBBTaskManagerGetCompletionBlock)completion
+{
+    NSMutableDictionary *headers = [NSMutableDictionary dictionary];
+    [self.authManager addAuthHeaderToHeaders:headers];
+    return [self.networkManager get:@"/api/v1/tasks" headers:headers parameters:nil completion:^(NSURLSessionDataTask *task, id responseObject, NSError *error) {
+        // temporary, until "type" field is added to response from this api
+        if ([responseObject isKindOfClass:[NSDictionary class]] && ![responseObject objectForKey:@"type"]) {
+            responseObject = [responseObject mutableCopy];
+            [responseObject setObject:@"ResourceList" forKey:@"type"];
+        }
+        SBBResourceList *tasks = [self.objectManager objectFromBridgeJSON:responseObject];
+        if (completion) {
+            completion(tasks, error);
+        }
+    }];
+}
+
+- (NSURLSessionDataTask *)startTask:(SBBTask *)task asOf:(NSDate *)startDate withCompletion:(SBBTaskManagerUpdateCompletionBlock)completion
+{
+    task.startedOn = startDate;
+    [self updateTasks:@[task] withCompletion:completion];
+}
+
+- (NSURLSessionDataTask *)finishTask:(SBBTask *)task asOf:(NSDate *)finishDate withCompletion:(SBBTaskManagerUpdateCompletionBlock)completion
+{
+    task.finishedOn = finishDate;
+    [self updateTasks:@[task] withCompletion:completion];
+}
+
+- (NSURLSessionDataTask *)deleteTask:(SBBTask *)task withCompletion:(SBBTaskManagerUpdateCompletionBlock)completion
+{
+    [self finishTask:task asOf:[NSDate date] withCompletion:completion];
+}
+
+- (NSURLSessionDataTask *)updateTasks:(NSArray *)tasks withCompletion:(SBBTaskManagerUpdateCompletionBlock)completion
+{
+    NSMutableArray *jsonTasks = [NSMutableArray arrayWithCapacity:tasks.count];
+    for (SBBTask *task in tasks) {
+        NSMutableDictionary *jsonTask = [NSMutableDictionary dictionary];
+        [jsonTask setObject:task.type forKey:@"type"];
+        [jsonTask setObject:task.guid forKey:@"guid"];
+        if (task.startedOn) {
+            [jsonTask setObject:[task.startedOn ISO8601String] forKey:@"startedOn"];
+        }
+        if (task.finishedOn) {
+            [jsonTask setObject:[task.finishedOn ISO8601String] forKey:@"finishedOn"];
+        }
+    }
+    NSMutableDictionary *headers = [NSMutableDictionary dictionary];
+    [self.authManager addAuthHeaderToHeaders:headers];
+    return [self.networkManager post:@"/api/v1/profile" headers:headers parameters:jsonProfile completion:^(NSURLSessionDataTask *task, id responseObject, NSError *error) {
+        if (completion) {
+            completion(responseObject, error);
+        }
+    }];
+}
+
+@end
