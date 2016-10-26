44792856a53826997116d009dd15a47a8876d05e
Erin Mounts <erinys62@gmail.com>
2016-10-26 06:05:41 -0700

    add cache manager integration tests
    stub, wip

---
+++ BridgeSDKTests/SBBCacheManagerIntegrationTests.m
@@ -0,0 +1,31 @@
+//
+//  SBBCacheManagerIntegrationTests.m
+//  BridgeSDK
+//
+//  Created by Erin Mounts on 10/25/16.
+//  Copyright © 2016 Sage Bionetworks. All rights reserved.
+//
+
+#import "SBBBridgeAPIIntegrationTestCase.h"
+#import "SBBCacheManager.h"
+
+@interface SBBCacheManagerIntegrationTests : SBBBridgeAPIIntegrationTestCase
+
+@end
+
+@implementation SBBCacheManagerIntegrationTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testProfileAndDataGroupsAcrossSignIn {
+}
+
+@end
