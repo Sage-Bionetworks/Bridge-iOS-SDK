dc7c7986492461a4a4df7e5e1eec786d4389910c
Khai Do <zaro0508@gmail.com>
2017-02-22 22:09:04 -0800

    Setup for CI
    Add files for CI builds:
    * fastlane - for running xcode command line builds
    * travis - for Travis CI

---
+++ travis/install-fastlane.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+set -ex
+brew cask install fastlane
+gem update fastlane
---
+++ travis/run-tests.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -ex
+# show available schemes
+xcodebuild -list -project ./BridgeSDK.xcodeproj
+# run on pull request
+if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+  fastlane test scheme:"BridgeSDK"
+  exit $?
+fi
