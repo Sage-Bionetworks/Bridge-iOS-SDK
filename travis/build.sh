486b1d37d641fca5ecdcba402dfb8331cf29969b
Khai Do <zaro0508@gmail.com>
2017-04-13 14:31:51 -0700

    Use travis ENV vars instead of encrypting file

--- travis/build.sh
+++ travis/build.sh
@@ -7,8 +7,6 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
 fi
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
     bundle exec fastlane doc scheme:"BridgeSDK"
-    openssl aes-256-cbc -K $encrypted_d59c41fdd72b_key -iv $encrypted_d59c41fdd72b_iv -in BridgeAdminCredentials.plist.enc -out BridgeAdminCredentials.plist -d
-    mv BridgeAdminCredentials.plist ..
     bundle exec fastlane test scheme:"BridgeSDK"
 fi
 exit $?
