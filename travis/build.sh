bf045fa85b57f0bf79141c0e6299adb59339a277
Khai Do <zaro0508@gmail.com>
2017-04-12 22:57:15 -0700

    Setup encrypted BridgeAdminCredentials.plist for tests
    Encrypt BridgeAdminCredentials.plist and decrypt it to
    allow tests to run.

--- travis/build.sh
+++ travis/build.sh
@@ -3,10 +3,9 @@ set -ex
 # show available schemes
 # xcodebuild -list -project ./BridgeSDK.xcodeproj
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-    # disable tests due to issue BRIDGE-1727
-    # bundle exec fastlane test scheme:"BridgeSDK"
+    bundle exec fastlane test scheme:"BridgeSDK"
 elif [ "$TRAVIS_BRANCH" = "master" ]; then
-    # bundle exec fastlane test scheme:"BridgeSDK"
+    bundle exec fastlane test scheme:"BridgeSDK"
 fi
 if [ ! -z "$TRAVIS_TAG" ]; then
     bundle exec fastlane doc scheme:"BridgeSDK"
