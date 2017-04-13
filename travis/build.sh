471b147aa933c65ad0d2dc17791b952b1f63b6fd
Khai Do <zaro0508@gmail.com>
2017-04-12 23:40:30 -0700

    encrypted data is not available to pull requests

--- travis/build.sh
+++ travis/build.sh
@@ -3,11 +3,12 @@ set -ex
 # show available schemes
 # xcodebuild -list -project ./BridgeSDK.xcodeproj
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-    bundle exec fastlane test scheme:"BridgeSDK"
-elif [ "$TRAVIS_BRANCH" = "master" ]; then
-    bundle exec fastlane test scheme:"BridgeSDK"
-fi
-if [ ! -z "$TRAVIS_TAG" ]; then
     bundle exec fastlane doc scheme:"BridgeSDK"
+fi
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+    bundle exec fastlane doc scheme:"BridgeSDK"
+    openssl aes-256-cbc -K $encrypted_d59c41fdd72b_key -iv $encrypted_d59c41fdd72b_iv -in BridgeAdminCredentials.plist.enc -out BridgeAdminCredentials.plist -d
+    mv BridgeAdminCredentials.plist ..
+    bundle exec fastlane test scheme:"BridgeSDK"
 fi
 exit $?
