688db3156d189a8327900f4b4a1852a2fd789d41
Erin Mounts <erinys62@gmail.com>
2017-05-16 11:38:42 -0700

    update consent manager integration tests
    also suppress deprecation warnings in integration tests

--- BridgeSDKTests/SBBConsentManagerIntegrationTests.m
+++ BridgeSDKTests/SBBConsentManagerIntegrationTests.m
@@ -22,6 +22,9 @@
 
 @implementation SBBConsentManagerIntegrationTests
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+
 - (void)setUp {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -109,23 +112,24 @@
     NSDate *birthdate = [NSDate dateWithTimeIntervalSinceNow:-(30 * 365.25 * 86400)];
     
     [self createTestUserConsented:NO roles:@[] completionHandler:^(NSString *emailAddress, NSString *password, id responseObject, NSError *error) {
+        XCTAssert(!error, @"Error creating unconsented user %@:\n%@\nResponse: %@", unconsentedEmail, error, responseObject);
         if (error) {
-            NSLog(@"Error creating unconsented user %@:\n%@\nResponse: %@", unconsentedEmail, error, responseObject);
             [expectSigned fulfill];
         } else {
             unconsentedEmail = emailAddress;
             NSSet *dataGroups = [NSSet setWithArray:@[@"sdk-int-1", @"sdk-int-2"]];
             [aMan signInWithEmail:emailAddress password:password completion:^(NSURLSessionTask *task, id responseObject, NSError *error) {
-                if (error && error.code != SBBErrorCodeServerPreconditionNotMet) {
-                    NSLog(@"Error signing in unconsented user %@:\n%@\nResponse: %@", unconsentedEmail, error, responseObject);
+                BOOL relevantError = (error && error.code != SBBErrorCodeServerPreconditionNotMet);
+                XCTAssert(!relevantError, @"Error signing in unconsented user %@:\n%@\nResponse: %@", unconsentedEmail, error, responseObject);
+                if (relevantError) {
                     [expectSigned fulfill];
                 } else {
                     unconsentedId = responseObject[kUserSessionInfoIdKey];
                     // sign default (required) consent first
                     SBBParticipantDataSharingScope scope = SBBParticipantDataSharingScopeStudy;
                     [cMan consentSignature:signname forSubpopulationGuid:[SBBBridgeInfo shared].studyIdentifier birthdate:birthdate signatureImage:signatureImage dataSharing:scope completion:^(id  _Nullable responseObject, NSError * _Nullable error) {
+                        XCTAssert(!error, @"Error recording default consent signature:\n%@\nResponse: %@", error, responseObject);
                         if (error) {
-                            NSLog(@"Error recording default consent signature:\n%@\nResponse: %@", error, responseObject);
                             [expectSigned fulfill];
                         } else {
                             // verify that the cached participant manager got updated
@@ -136,18 +140,15 @@
                             }
                             // add to subpopulation that has its own required consent
                             [pMan addToDataGroups:dataGroups completion:^(id responseObject, NSError *error) {
-                                if (error && error.code != SBBErrorCodeServerPreconditionNotMet) {
-                                    NSLog(@"Error adding user %@ to groups %@:\n%@\nResponse: %@", unconsentedEmail, dataGroups, error, responseObject);
+                                BOOL relevantError = (error && error.code != SBBErrorCodeServerPreconditionNotMet);
+                                XCTAssert(!relevantError, @"Error adding user %@ to groups %@:\n%@\nResponse: %@", unconsentedEmail, dataGroups, error, responseObject);
+                                if (relevantError) {
                                     [expectSigned fulfill];
                                 } else {
                                     // sign subpopulation consent
                                     [cMan consentSignature:signname forSubpopulationGuid:self.testSubpopRequiredGuid birthdate:birthdate signatureImage:signatureImage dataSharing:SBBParticipantDataSharingScopeStudy completion:^(id responseObject, NSError *error) {
-                                        if (error) {
-                                            NSLog(@"Error recording subpop consent signature:\n%@\nResponse: %@", error, responseObject);
-                                        } else {
-                                            consentSigned = YES;
-                                        }
-                                        XCTAssert(!error, @"Successfully recorded subpop consent signature");
+                                        XCTAssert(!error, @"Error recording subpop consent signature:\n%@\nResponse: %@", error, responseObject);
+                                        consentSigned = !error;
                                         [expectSigned fulfill];
                                     }];
                                 }
@@ -166,6 +167,16 @@
     }];
     
     if (consentSigned) {
+        // check that UserSessionInfo was updated with the new consent status
+        SBBUserSessionInfo *sessionInfo = (SBBUserSessionInfo *)[SBBComponent(SBBCacheManager) cachedSingletonObjectOfType:SBBUserSessionInfo.entityName createIfMissing:NO];
+        XCTAssert(sessionInfo, @"Failed to retrieve updated UserSessionInfo from cache");
+        if (sessionInfo) {
+            SBBConsentStatus *status = sessionInfo.consentStatuses[self.testSubpopRequiredGuid];
+            XCTAssert(status, @"Failed to retrieve ConsentStatus for %@", self.testSubpopRequiredGuid);
+            if (status) {
+                XCTAssert(status.consentedValue, @"Cached ConsentStatus for %@ does not reflect having consented", self.testSubpopRequiredGuid);
+            }
+        }
         // now try to get it back
         XCTestExpectation *expectReceived = [self expectationWithDescription:@"consent signature received"];
         
@@ -272,5 +283,7 @@
         NSLog(@"Failed to retrieve test account id for %@, account not deleted", consentedEmail);
     }
 }
+
+#pragma clang diagnostic pop
 
 @end
