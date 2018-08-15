0ccf03d1ba339e183de2eeff7081b263c305d01f
Khai Do <zaro0508@gmail.com>
2018-08-15 15:53:26 -0700

    don't build and run tests

--- travis/build.sh
+++ travis/build.sh
@@ -4,10 +4,10 @@ set -ex
 cp BridgeAdminCredentials.plist ../BridgeAdminCredentials.plist
 # build docs
 bundle exec fastlane doc scheme:"BridgeSDK"
-if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then     # on pull requests
-    # secrets not available on PR builds so need to exclude integration tests
-    FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane test scheme:"BridgeSDK" only_testing:"BridgeSDKTests"
-elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" == "master" ]]; then  # non-tag commits to master branch
-    FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane test scheme:"BridgeSDK" # run all tests
-fi
+#if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then     # on pull requests
+#    # secrets not available on PR builds so need to exclude integration tests
+#    FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane test scheme:"BridgeSDK" only_testing:"BridgeSDKTests"
+#elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" == "master" ]]; then  # non-tag commits to master branch
+#    FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane test scheme:"BridgeSDK" # run all tests
+#fi
 exit $?
