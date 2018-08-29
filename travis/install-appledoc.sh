dbf4c214db016a80a5565a647f442ec4e8ce30e2
Khai Do <zaro0508@gmail.com>
2018-08-29 13:23:42 -0700

    [BRIDGE-2282] fix apple doc generation
    The appledoc version from brew doesn't generate all files[1].
    Using the latest version from Github fixes this issue.
    
    [1] https://github.com/tomaz/appledoc/issues/596

--- travis/install-appledoc.sh
+++ travis/install-appledoc.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 set -ex
-brew install appledoc
+# version from brew is borked, https://github.com/tomaz/appledoc/issues/596
+# brew install appledoc
+git clone git://github.com/tomaz/appledoc.git ~/appledoc
+pushd ~/appledoc
+./install-appledoc.sh -t ~/.appledoc
+popd
+
